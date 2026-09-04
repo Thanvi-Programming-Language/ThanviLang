e7024ae42bf6ee6ce21b3f739941ef4ccd41262d
Meeravali Velupuri <113295706+MeeravaliVelupuri@users.noreply.github.com>
2026-09-04 19:07:47 +0530

    Add files via upload

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,1 +1,4 @@
-
+from runtime import run
+def test_basic():assert run('show 10 + 20\nfinish')=='30'
+def test_array():assert run('set x = [1,2,3]\nshow x[1]\nfinish')=='2'
+def test_function():assert run('define add(a,b) =>\n give a+b\nend\nshow add(2,3)\nfinish')=='5'
